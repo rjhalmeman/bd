08a5e3e1c13361573c2692cda08c83ead8a7e870
Radames <rjhalmeman@gmail.com>
2024-08-13 09:04:40 -0300

    Update scriptSFM_v001.sql

--- 3 - SalaoFazMilagre/scriptSFM_v001.sql
+++ 3 - SalaoFazMilagre/scriptSFM_v001.sql
@@ -1,16 +1,11 @@
-CREATE SCHEMA salaoFazMilagre;
-USE salaoFazMilagre;
+CREATE DATABASE IF NOT EXISTS SFM;
+USE SFM;
 
 CREATE TABLE Pessoa (
     cpfPessoa VARCHAR(11) PRIMARY KEY,
     nomePessoa VARCHAR(100),
-    senhaPessoa VARCHAR(20)
-);
-
-CREATE TABLE Funcionario (
-    pessoaCpfPessoa VARCHAR(11),
-    PRIMARY KEY (pessoaCpfPessoa),
-    FOREIGN KEY (pessoaCpfPessoa) REFERENCES Pessoa(cpfPessoa)
+    contatoPessoa VARCHAR(100),
+    enderecoPessoa VARCHAR(255)
 );
 
 CREATE TABLE Cliente (
@@ -19,15 +14,38 @@ CREATE TABLE Cliente (
     FOREIGN KEY (pessoaCpfPessoa) REFERENCES Pessoa(cpfPessoa)
 );
 
-CREATE TABLE Servico (
-    idServico INT PRIMARY KEY,
-    nomeServico VARCHAR(45)
+CREATE TABLE Cargo (
+    idCargo INT AUTO_INCREMENT,
+    nomeCargo VARCHAR(45),
+    PRIMARY KEY (idCargo)
+);
+
+CREATE TABLE Funcionario (
+    pessoaCpfPessoa VARCHAR(11),
+    cargoIdCargo INT,
+    PRIMARY KEY (pessoaCpfPessoa),
+    FOREIGN KEY (pessoaCpfPessoa) REFERENCES Pessoa(cpfPessoa),
+    FOREIGN KEY (cargoIdCargo) REFERENCES Cargo(idCargo)
 );
 
 CREATE TABLE Horario (
     siglaHorario VARCHAR(2) PRIMARY KEY,
     inicioHorario TIME,
     fimHorario TIME
+);
+
+CREATE TABLE Servico (
+    idServico INT AUTO_INCREMENT,
+    nomeServico VARCHAR(45),
+    PRIMARY KEY (idServico)
+);
+
+CREATE TABLE FuncionarioHasServico (
+    funcionarioPessoaCpfPessoa VARCHAR(11),
+    servicoIdServico INT,
+    PRIMARY KEY (funcionarioPessoaCpfPessoa, servicoIdServico),
+    FOREIGN KEY (funcionarioPessoaCpfPessoa) REFERENCES Funcionario(pessoaCpfPessoa),
+    FOREIGN KEY (servicoIdServico) REFERENCES Servico(idServico)
 );
 
 CREATE TABLE Reserva (
