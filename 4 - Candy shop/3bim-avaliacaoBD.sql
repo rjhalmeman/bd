5f2beaeb42119ef8c702cb9ea6b104c88dcf94df
Radames <rjhalmeman@gmail.com>
2024-10-15 07:21:29 -0300

    15/10/2024 - 07:21:29

---
+++ 4 - Candy shop/3bim-avaliacaoBD.sql
@@ -0,0 +1,31 @@
+use CandyShop;
+
+-- 1 O pedido 5 (idPedido = 5) tem vários itens. Foi calculado que o valor -- a  ser pago é 168,5. Qual a consulta (sql select) que foi utilizada para
+-- calcular esse valor? Quais as formas de pagamento foram utilizadas (deve -- aparecer o nome da forma de pagamento) e qual o valor de cada uma?
+-- (mostre o sql select). A resposta tem 2 consultas independentes uma da -- outra.
+
+
+-- 1a
+
+
+-- 1b
+
+
+
+
+-- 2 Mostre o sql select que mostra qual a comissão recebida pelo 
+-- funcionário que atendeu e efetuou o pedido 5 (idPedido = 5)
+
+
+-- 3 Faça o sql select que totalize todas as vendas do mês 10 (considere 
+-- venda o pedido que foi pago).
+
+
+-- 4 Faça uma listagem (sql select) que mostre quantas vezes os produtos 
+-- apareceram em diferentes pedidos
+-- (não importa quantas unidades do produto e sim quantas vezes esse
+-- produto apareceu em diferentes pedidos pagos). A ordem é do mais vendido -- para o menos vendido.
+
+
+-- 5 Qual o sql select que responde a pergunta: qual o peso do pedido 7?
+-- Lembre-se que todos os produtos são medidos em quilogramas
